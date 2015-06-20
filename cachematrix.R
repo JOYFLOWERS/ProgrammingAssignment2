9297290ad22a8948d45067e5425094267ab5a654
Joy Flowers <joyflowers123@gmail.com>
2015-06-19 23:07:43 -0700

    Asgn 2 w Comments Final

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,46 @@
 ## R Programming JHU - Coursera
 ## Programming Assignment 2
 ## Joy Flowers   June 19, 2015
+## This program illustrates lexical scoping within the R programming language.
+## The user calls the makeCacheMatrix function, passing a matrix to it, such as: 
+## inmatrix <- makeCacheMatrix(matrix(c(2,-1,1,4,1,4,1,-1,0), nrow=3, ncol=3))
+## The function makeCacheMatrix creates a list containing four functions:
+## setmatrix, getmatrix, setinverse, getinverse which can all be accessed 
+## and run at the console. 
+## The program also illustrates how to cache the inverse of the matrix, as the computation
+## can be costly in terms of performance. Once the inverse of the matrix has been
+## computed one time, it is cached into memory, so that the next time it is simply
+## retrieved from cache. The cacheSolve function checks to see if the inverse has
+## been stored in cache. If so, it retrieves the information from cache, otherwise it 
+## makes the inverse matrix computation. It can be called as cacheSolve(inmatrix)
+## The function getmatrix returns the matrix , such as inmatrix$getmatrix()
+## The function setmatrix sets up a new matrix
+## The function getinverse returns the inverse of the matrix
+## The function setinverse assigns the inverse matrix and is called by cacheSolve.
+## The program checks to make sure the matrix is square, and the determinant is not 0
+## which are necessary criterion in order for the matrix to have an inverse.
+## Example: Once inmatrix <- makeCacheMatrix(matrix(c(2,-1,1,4,1,4,1,-1,0), nrow=3, ncol=3)) is called, then
+## inmatrix$getmatrix() returns the following:
+##         [,1] [,2] [,3]
+##  [1,]    2    4    1
+##  [2,]   -1    1   -1
+##  [3,]    1    4    0
+##  
+## cacheSolve(inmatrix) returns the inverse of the matrix such as:
+##           [,1] [,2] [,3]
+##     [1,]   -4   -4    5
+##     [2,]    1    1   -1
+##     [3,]    5    4   -6
+## After the inverse has been set (within the cacheSolve function),
+## the function inmatrix$getinverse() returns the inverse.
+## If the inverse matrix has been cached, a message will say it is retrieving from cache.
+## The matrix elements and dimensions can be reset using inmatrix$setmatrix(matrix(c(2,3,4,3),2,2))
+## and the whole process can start again, for example using inmatrix$getmatrix() or cacheSolve(inmatrix)
+## 
+
+## The function makeCacheMatrix is passed a matrix, and it creates a list containing four functions:
+## setmatrix, getmatrix, setinverse, getinverse which can all be accessed 
+## and run at the console. 
 
 makeCacheMatrix <- function(x = matrix()) {
         if(nrow(x) != ncol(x)) {
